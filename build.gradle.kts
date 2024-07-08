617f7cbbe4e4509f4b598015edb2a8c9519b2cb7
tufeigunchu <88198403+tufeigunchu@users.noreply.github.com>
2024-07-08 10:29:23 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.10.24")
+project.ext.set("kernelPatchVersion", "0.11.0-dev")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 34
